b19b69c4be6867868175578221fe6594ab08e0d0
Michal Tomczyk <michaltomczyk28@gmail.com>
2022-11-24 23:32:17 +0100

    [LINKED LIST] Implement linked list with push, pop, shift and unshift

---
+++ linked_list/main.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, value, next = None) -> None:
+        self.value = value
+        self.next = next
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+    
+    def unshift(self, value):
+        self.head = Node(value, self.head)
+
+    def shift(self):
+        self.head = self.head.next
+
+    def push(self, value):
+        newNode = Node(value)
+
+        if self.head == None:
+            self.head = newNode
+            return
+
+        current = self.head
+        while current.next != None:
+            current = current.next
+        
+        current.next = newNode
+
+    def pop(self):
+        if self.head == None:
+            return
+
+        current = self.head
+
+        while current.next != None and current.next.next != None:
+            current = current.next
+        
+        current.next = None
+
+    def __str__(self):
+        output = ""
+        current = self.head
+        
+        while current != None:
+            output += str(current.value) + ", "
+            current = current.next
+        
+        return output
+
+
+linked_list = LinkedList()
+
+linked_list.push(1)
+print(linked_list)
+
+linked_list.push(2)
+print(linked_list)
+
+linked_list.unshift(0)
+print(linked_list)
+
+linked_list.shift()
+print(linked_list)
+
+linked_list.pop()
+print(linked_list)
+ 
